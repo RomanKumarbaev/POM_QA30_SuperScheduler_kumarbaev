0ccb35cdc54e402fe97f2d8c0c9f34fe6cab7a24
Roman Kumarbaev <91917230+RomanKumarbaev@users.noreply.github.com>
2021-12-03 17:18:54 +0200

    Work

---
+++ src/main/java/config/ConfigurationScheduler.java
@@ -0,0 +1,36 @@
+package config;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class ConfigurationScheduler {
+    protected static AppiumDriver<MobileElement> driver;
+
+    @BeforeMethod
+    public  void setUp() throws MalformedURLException {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("platformName", "Android");
+        capabilities.setCapability("deviceName", "Nex");
+        capabilities.setCapability("platformVersion", "8.0");
+        capabilities.setCapability("appPackage", "com.example.svetlana.scheduler");
+        capabilities.setCapability("appActivity", ".presentation.splashScreen.SplashScreenActivity");
+
+        capabilities.setCapability("automationName", "Appium");
+        capabilities.setCapability("app","C:\\Users\\Comp.ROMAN\\Downloads\\v.0.0.3.apk");
+
+        driver = new AppiumDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"),capabilities);
+
+    }
+
+    @AfterMethod
+    public void tearDown(){
+
+        driver.quit();
+    }
+}
---
+++ src/test/java/scheduler/testMy.java
@@ -0,0 +1,12 @@
+package scheduler;
+
+import config.ConfigurationScheduler;
+import org.testng.annotations.Test;
+
+public class testMy extends ConfigurationScheduler {
+
+    @Test
+    public void testR(){
+
+    }
+}
